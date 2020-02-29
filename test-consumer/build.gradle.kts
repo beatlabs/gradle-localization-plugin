c381370199ea91a78e8f516c2cc9c9e5a0c01483
Chris Margonis <chris.margonis@gmail.com>
2020-02-29 22:36:31 +0200

    build: Switch to using nullable properties in consume build file

--- test-consumer/build.gradle.kts
+++ test-consumer/build.gradle.kts
@@ -2,14 +2,14 @@ plugins {
     id("co.thebeat.localization") version "0.3.6"
 }
 
-val apiKey: String by project
-val resource: String by project
-val projectSlugConfig: String by project
+val apiKey: String? by project
+val resource: String? by project
+val projectSlugConfig: String? by project
 
 transifexLocalization {
-    auth = apiKey
-    resourceSlug = resource
-    projectSlug = projectSlugConfig
+    auth = apiKey ?: ""
+    resourceSlug = resource ?: ""
+    projectSlug = projectSlugConfig ?: ""
 
     localesMap = HashMap<String, String>().apply {
         this["main/res/localization_en"] = "en"
